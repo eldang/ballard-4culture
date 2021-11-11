ee88e83cfc1e9da2ae74a2d038f3228e70b6a42a
Eldan Goldenberg <eg@eldang.xyz>
2021-11-11 15:32:11 -0800

    no functional change, just indentation as I familiarise myself with the code and my text editor adds an extra layer of pettiness

--- scripts/onload.js
+++ scripts/onload.js
@@ -20,25 +20,25 @@ var originalZoomLevel = map.getZoom();
 
 /* add places data from derived geojson*/
 
-			map.on('load', () => {
-			map.addSource('places', {
-			type: 'geojson',
-			// Use a URL for the value for the `data` property.
-			data: 'data/places.geojson'
-			});
-			 
-			map.addLayer({
-			'id': 'places-layer',
-			'type': 'circle',
-			'source': 'places',
-			'paint': {
+map.on('load', () => {
+	map.addSource('places', {
+		type: 'geojson',
+		// Use a URL for the value for the `data` property.
+		data: 'data/places.geojson'
+	});
+
+	map.addLayer({
+		'id': 'places-layer',
+		'type': 'circle',
+		'source': 'places',
+		'paint': {
 			'circle-radius': 8,
 			'circle-stroke-width': 2,
 			'circle-color': '#423493',
 			'circle-stroke-color': 'white'
-			}
-			});
-		});
+		}
+	});
+});
 /*
 *****************************************************
 This is where we will put code for the popups for the point layer(s)
@@ -51,32 +51,33 @@ the location of the click, with description HTML from its properties
 // location of the feature, with description HTML from its properties.
 
 map.on('click', 'places-layer', (e) => {
-		// Copy coordinates array.
-		const coordinates = e.features[0].geometry.coordinates.slice();
-		/*const address = e.features[0].properties.address;
-		const id = e.features[0].properties.id;*/
-		var p = e.features[0].properties;
-		 
-		// Ensure that if the map is zoomed out such that multiple
-		// copies of the feature are visible, the popup appears
-		// over the copy being pointed to.
-		while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+	// Copy coordinates array.
+	const coordinates = e.features[0].geometry.coordinates.slice();
+	/*const address = e.features[0].properties.address;
+	const id = e.features[0].properties.id;*/
+	var p = e.features[0].properties;
+	console.log(p);
+
+	// Ensure that if the map is zoomed out such that multiple
+	// copies of the feature are visible, the popup appears
+	// over the copy being pointed to.
+	while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
 		coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-		}
-		 
-		 //right now p.address is a placeholder for will ultimately be the Person's name
-		 //similarly, all other attributes will be styled with h4 in the css
-		new mapboxgl.Popup()
-					.setLngLat(coordinates)
-					.setHTML("<h3>" + p.address + '</h3><p><h4>'  + p.id + '</h4></p>')
-					.addTo(map);
-					});
- 
+	}
+
+ //right now p.address is a placeholder for will ultimately be the Person's name
+ //similarly, all other attributes will be styled with h4 in the css
+	new mapboxgl.Popup()
+		.setLngLat(coordinates)
+		.setHTML("<h3>" + p.address + '</h3><p><h4>'  + p.id + '</h4></p>')
+		.addTo(map);
+});
+
 // Change the cursor to a pointer when the mouse is over the places layer.
 map.on('mouseenter', 'places-layer', () => {
 	map.getCanvas().style.cursor = 'pointer';
 });
- 
+
 // Change it back to a pointer when it leaves.
 map.on('mouseleave', 'places-layer', () => {
 	map.getCanvas().style.cursor = '';
